9004ba058197372d77bbf868902dec377547e7b2
Mus <favmake@gmail.com>
2025-11-28 23:55:22 +0200

    initial project setup

---
+++ app/src/main/java/knu/mus/cryptocurrency_exchange_rate_viewer/MainActivity.kt
@@ -0,0 +1,15 @@
+package knu.mus.cryptocurrency_exchange_rate_viewer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
